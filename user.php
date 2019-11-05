1a5da0b9f94e0a7d6ac09bbb03a6905dcb133384
Utoplion <56998957+Utoplion@users.noreply.github.com>
2019-11-05 16:09:12 +0100

    jai resolu le bug de la connection par url exterieur

--- user.php
+++ user.php
@@ -1,8 +1,18 @@
 <?php
 session_start();
 
+if (!isset($_SESSION['id']))
+{
+   header('Location: index.php');
+   exit();
+}
+
 $bdd = new PDO("mysql:host=mysql-utoplion.alwaysdata.net;dbname=utoplion_13", 'utoplion_test', 'utoplion_13600');
 
+   $getid = intval($_GET['id']);
+   $requser = $bdd->prepare('SELECT * FROM connect WHERE id = ?');
+   $requser->execute(array($getid));
+   $userinfo = $requser->fetch();
 
 ?>
 <!DOCTYPE html>
@@ -12,7 +22,7 @@ $bdd = new PDO("mysql:host=mysql-utoplion.alwaysdata.net;dbname=utoplion_13", 'u
   <title>Page d'aceuille</title>
 </head>
 <body>
-  <h1>BIENVENUE SUR  VOTRE PAGE PERSO </h1>
+  <h1>BIENVENUE SUR  VOTRE PAGE PERSO <?php echo $userinfo['email']; ?> </h1>
 
 
 
